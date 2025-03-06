9ef853a6dec45ab598b91c0f1454c235ccaca5d8
mmazitov <mazitov.nikolya@gmail.com>
2025-03-06 18:42:11 +0200

    CI/CD test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        Welcome
+    <div className="justify-items-center items-center gap-16 grid grid-rows-[20px_1fr_20px] p-8 sm:p-20 pb-20 min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col items-center sm:items-start gap-8 row-start-2">
+        Welcome Page
       </main>
     </div>
   );
