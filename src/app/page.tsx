d7e9ccc157ed2a347e652521c684c48d4beab874
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2025-03-11 17:16:17 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        Welcome Page
+        Welcome Page  DEVELOPMENT
         <div className="mb-4 flex items-center gap-16">
           <Button>Для переменных</Button>
           <Button variant="white">Белая по умолчанию</Button>
