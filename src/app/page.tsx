e1025f3e4b427cbc028ebb403087bbb6fce1f471
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2025-03-15 16:40:06 +0200

    +

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import {Button} from '@components/ui'
 
 
 export default function Home() {
-
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
@@ -13,7 +12,7 @@ export default function Home() {
         <div className="mb-4 flex items-center gap-16">
           <Button>Primary</Button>
           <Button variant="secondary">Secondary</Button>
-          <Button variant="secondary" disabled>Disabled</Button>
+          <Button variant="secondary" disabled>isabled</Button>
         </div>
         {/*<div className="mb-4 flex items-center gap-16">*/}
         {/*  <Input*/}
