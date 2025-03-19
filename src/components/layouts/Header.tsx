4f606ccfc7c7390cd8fa9ddce057bd35e4642028
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2025-03-19 14:55:34 +0100

    add checkbox & radio

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -24,6 +24,7 @@ export function Header() {
 
   const handleChange = (checked: boolean) => {
     setCheckedValue(checked);
+    console.log("checkedValue :>> ", checkedValue);
   };
 
   const handleRadioChange = (value: string) => {
