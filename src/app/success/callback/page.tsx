c51bc12564d0e598f05f3c5d44c5abee53b25599
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2025-04-25 23:05:55 +0200

    change googlecallback page

--- src/app/success/callback/page.tsx
+++ src/app/success/callback/page.tsx
@@ -1,31 +1,71 @@
+// "use client";
+
+// import { fetchGoogleCallback } from "@/lib/redux/auth/operations";
+// import { selectIsAuthenticated } from "@/lib/redux/auth/selectors";
+// import { useAppDispatch, useAppSelector } from "@/lib/redux/hooks";
+// import { useRouter } from "next/navigation";
+// import React, { useEffect } from "react";
+
+// const Page = () => {
+//   const router = useRouter();
+//   const dispatch = useAppDispatch();
+//   const isAuthenticated = useAppSelector(selectIsAuthenticated);
+
+//   useEffect(() => {
+//     const code = new URLSearchParams(window.location.search).get("code");
+
+//     if (code && !isAuthenticated) {
+//       dispatch(fetchGoogleCallback(code));
+//     }
+//   }, [dispatch, isAuthenticated]);
+
+//   useEffect(() => {
+//     if (isAuthenticated) router.push("/");
+//   }, [router, isAuthenticated]);
+
+//   return (
+//     <div className="flex h-screen items-center justify-center">
+//       <p>Вхід через Google...</p>
+//     </div>
+//   );
+// };
+
+// export default Page;
+
 "use client";
 
+import { useMutation } from "@tanstack/react-query";
 import { fetchGoogleCallback } from "@/lib/redux/auth/operations";
 import { selectIsAuthenticated } from "@/lib/redux/auth/selectors";
 import { useAppDispatch, useAppSelector } from "@/lib/redux/hooks";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React from "react";
 
 const Page = () => {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const isAuthenticated = useAppSelector(selectIsAuthenticated);
+  const code =
+    typeof window !== "undefined"
+      ? new URLSearchParams(window.location.search).get("code")
+      : null;
 
-  useEffect(() => {
-    const code = new URLSearchParams(window.location.search).get("code");
+  const mutation = useMutation({
+    mutationFn: async (code: string) => {
+      await dispatch(fetchGoogleCallback(code));
+    },
+    onSuccess: () => {
+      router.push("/");
+    },
+  });
 
-    if (code && !isAuthenticated) {
-      dispatch(fetchGoogleCallback(code));
-    }
-  }, [dispatch, isAuthenticated]);
-
-  useEffect(() => {
-    if (isAuthenticated) router.push("/");
-  }, [router, isAuthenticated]);
+  if (code && !isAuthenticated && mutation.status === "idle") {
+    mutation.mutate(code);
+  }
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <p>Вхід через Google...</p>
+      <p>Виконуємо вхід через Google...</p>
     </div>
   );
 };
