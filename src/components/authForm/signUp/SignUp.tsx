dcd2fb10aa8fc5260dff6a8e5b572408abab2db9
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2025-05-01 21:40:55 +0300

    Refaktor Code

--- src/components/authForm/signUp/SignUp.tsx
+++ src/components/authForm/signUp/SignUp.tsx
@@ -1,93 +1,3 @@
-// "use client";
-// import {
-//   Button,
-//   Dialog,
-//   DialogContent,
-//   DialogFooter,
-//   DialogHeader,
-//   DialogTitle,
-// } from "@components/ui";
-// import { selectIsSignUp } from "@lib/redux/toggleModal/selectors";
-// import { closeModal, openModal } from "@lib/redux/toggleModal/slice";
-// import { SingUpForm } from "@components/authForm/signUp/SignUpForm";
-// import FormFooter from "@components/authForm/formFooter";
-// import Image from "next/image";
-// import { useEffect } from "react";
-// import { selectAuthError, selectIdUser } from "@lib/redux/auth/selectors";
-// import { clearError } from "@lib/redux/auth/slice";
-// import { notify } from "@components/notifi/notifi";
-// import { useAppDispatch, useAppSelector } from "@/lib/redux/hooks";
-
-// export function SignUp() {
-//   const isSignUp = useAppSelector(selectIsSignUp);
-//   const userId = useAppSelector(selectIdUser);
-//   const dispatch = useAppDispatch();
-//   const IsError = useAppSelector(selectAuthError);
-
-//   useEffect(() => {
-//     if (userId !== "") {
-//       notify({
-//         message: "На ваш email відправлено код підтвердження",
-//         type: "success",
-//       });
-//       dispatch(closeModal("isSignUp"));
-//       dispatch(openModal("isValidateOTP"));
-//     }
-//     if (IsError) {
-//       notify({ message: IsError, type: "error" });
-//       dispatch(clearError());
-//     }
-//   }, [IsError, userId, dispatch]);
-
-//   return (
-//     <>
-//       <Dialog
-//         open={isSignUp}
-//         onOpenChange={() => dispatch(closeModal("isSignUp"))}
-//       >
-//         <DialogContent
-//           aria-describedby={undefined}
-//           className="flex min-h-3/5 w-[90%] max-w-[1280px] flex-col gap-0 p-0 lg:min-h-4/5 lg:flex-row"
-//         >
-//           <div className="xl2:px-25 flex w-full flex-col justify-center gap-4 px-5 py-15 sm:px-20 lg:w-2/3 lg:px-20 xl:w-1/2">
-//             <DialogHeader>
-//               <DialogTitle className="h2-text text-[var(--text-black)]">
-//                 Реєстрація
-//               </DialogTitle>
-//             </DialogHeader>
-//             <SingUpForm />
-//             <DialogFooter className="gap-4">
-//               <Button
-//                 variant="link"
-//                 onClick={() => {
-//                   dispatch(closeModal("isSignUp"));
-//                   dispatch(openModal("isSignIn"));
-//                 }}
-//               >
-//                 <span className="body-text mr-1 text-[var(--text-grey)]">
-//                   Вже маєте аккаунт?
-//                 </span>
-//                 Увійти
-//               </Button>
-//               <FormFooter />
-//             </DialogFooter>
-//           </div>
-
-//           <div className="relative hidden lg:block lg:w-1/3 xl:w-1/2">
-//             <Image
-//               src="/image24.jpg"
-//               alt="Реєстрація"
-//               fill
-//               sizes="700px"
-//               style={{ objectFit: "cover" }}
-//               className="rounded-r-lg"
-//             />
-//           </div>
-//         </DialogContent>
-//       </Dialog>
-//     </>
-//   );
-// }
 "use client";
 import {
   Button,
@@ -102,32 +12,11 @@ import { closeModal, openModal } from "@lib/redux/toggleModal/slice";
 import { SingUpForm } from "@components/authForm/signUp/SignUpForm";
 import FormFooter from "@components/authForm/formFooter";
 import Image from "next/image";
-import { useEffect } from "react";
-import { selectAuthError, selectIdUser } from "@lib/redux/auth/selectors";
-import { clearError } from "@lib/redux/auth/slice";
-import { notify } from "@components/notifi/notifi";
 import { useAppDispatch, useAppSelector } from "@/lib/redux/hooks";
 
 export function SignUp() {
   const isSignUp = useAppSelector(selectIsSignUp);
-  const userId = useAppSelector(selectIdUser);
   const dispatch = useAppDispatch();
-  const IsError = useAppSelector(selectAuthError);
-
-  useEffect(() => {
-    if (userId) {
-      notify({
-        message: "На ваш email відправлено код підтвердження",
-        type: "success",
-      });
-      dispatch(closeModal("isSignUp"));
-      dispatch(openModal("isValidateOTP"));
-    }
-    if (IsError) {
-      notify({ message: IsError, type: "error" });
-      dispatch(clearError());
-    }
-  }, [IsError, userId, dispatch]);
 
   return (
     <>
--- src/lib/redux/auth/slice.ts
+++ src/lib/redux/auth/slice.ts
@@ -1,48 +1,6 @@
-import { createSlice, isAnyOf, PayloadAction } from "@reduxjs/toolkit";
-import {
-  // logInUser,
-  // logOut,
-  // registerUser,
-  // validateRegistrationEmail,
-  // resendValidationCode,
-  refreshAccessToken,
-  // recoveryPassword,
-  // changePassword,
-  // verifyEmail,
-  // fetchGoogleAuth,
-  // fetchGoogleCallback,
-} from "@lib/redux/auth/operations";
-import {
-  IAuthResponse,
-  IAuthState,
-  // IGoogleCallback,
-  // IGoogleResponse,
-  // IRegistrationResponse,
-} from "../types";
+import { createSlice } from "@reduxjs/toolkit";
 
-const handlePending = (state: IAuthState) => {
-  state.isLoading = true;
-  state.error = null;
-  state.isAuthenticated = false;
-};
-
-const handleRejected = (state: IAuthState, action: PayloadAction<unknown>) => {
-  state.isLoading = false;
-  state.isResend = false;
-  state.isAuthenticated = false;
-  state.error =
-    typeof action.payload === "string" ? action.payload : "Unknown error";
-};
-
-const setAuthSuccess = (
-  state: IAuthState,
-  action: PayloadAction<IAuthResponse>,
-) => {
-  state.accessToken = action.payload.access_token;
-  state.isLoading = false;
-  state.error = null;
-  state.isAuthenticated = true;
-};
+import { IAuthState } from "../types";
 
 const initialState: IAuthState = {
   accessToken: "",
@@ -104,108 +62,6 @@ const authSlice = createSlice({
       state.error = null;
     },
   },
-
-  extraReducers: (builder) => {
-    // builder.addCase(
-    //   registerUser.fulfilled,
-    //   (state: IAuthState, action: PayloadAction<IRegistrationResponse>) => {
-    //     state.isLoading = false;
-    //     state.error = null;
-    //     state.id = action.payload.id;
-    //   },
-    // );
-
-    // builder.addCase(logInUser.fulfilled, setAuthSuccess);
-
-    // builder.addCase(validateRegistrationEmail.fulfilled, setAuthSuccess);
-
-    // builder.addCase(resendValidationCode.fulfilled, (state: IAuthState) => {
-    //   state.isLoading = false;
-    //   state.error = null;
-    //   state.isResend = false;
-    // });
-
-    builder
-      .addCase(refreshAccessToken.fulfilled, setAuthSuccess)
-
-      // builder.addCase(
-      //   recoveryPassword.fulfilled,
-      //   (state: IAuthState, action: PayloadAction<IRegistrationResponse>) => {
-      //     state.isLoading = false;
-      //     state.error = null;
-      //     state.id = action.payload.id;
-      //   },
-      // );
-
-      // builder.addCase(verifyEmail.fulfilled, (state: IAuthState) => {
-      //   state.isLoading = false;
-      //   state.error = null;
-      //   state.isVerify = true;
-      // });
-
-      // builder
-      //   .addCase(changePassword.fulfilled, (state: IAuthState) => {
-      //     state.isLoading = false;
-      //     state.error = null;
-      //     state.isVerify = false;
-      //     state.id = "";
-      //     state.isPasswordChange = true;
-      //   })
-
-      // builder.addCase(
-      //   fetchGoogleAuth.fulfilled,
-      //   (state: IAuthState, action: PayloadAction<IGoogleResponse>) => {
-      //     state.isLoading = false;
-      //     state.error = null;
-      //     state.url = action.payload.url;
-      //   },
-      // );
-
-      // builder.addCase(
-      //   fetchGoogleCallback.fulfilled,
-      //   (state: IAuthState, action: PayloadAction<IGoogleCallback>) => {
-      //     state.isLoading = false;
-      //     state.error = null;
-      //     state.accessToken = action.payload.access_token;
-      //     state.isAuthenticated = true;
-      //   },
-      // );
-
-      // builder
-      //   .addCase(logOut.fulfilled, () => {
-      //     return initialState;
-      //   })
-
-      .addMatcher(
-        isAnyOf(
-          // registerUser.pending,
-          // logInUser.pending,
-          // validateRegistrationEmail.pending,
-          // resendValidationCode.pending,
-          refreshAccessToken.pending,
-          // changePassword.pending,
-          // verifyEmail.pending,
-          // recoveryPassword.pending,
-          // fetchGoogleAuth.pending,
-        ),
-        handlePending,
-      )
-      .addMatcher(
-        isAnyOf(
-          // registerUser.rejected,
-          // logInUser.rejected,
-          // logOut.rejected,
-          // validateRegistrationEmail.rejected,
-          // resendValidationCode.rejected,
-          refreshAccessToken.rejected,
-          // changePassword.rejected,
-          // verifyEmail.rejected,
-          // recoveryPassword.rejected,
-          // fetchGoogleAuth.rejected,
-        ),
-        handleRejected,
-      );
-  },
 });
 
 export const {
@@ -216,10 +72,11 @@ export const {
   addVerify,
   removeVerify,
   addPasswordChange,
+  // TODO Перевірити  removePasswordChange,
   removePasswordChange,
   resendEmail,
   removeResendEmail,
-  clearError,
+  clearError, // TODO Видалити
 } = authSlice.actions;
 
 export default authSlice.reducer;
